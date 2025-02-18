334967895e8673ba8256477ac1a5212d5b041824
Kgothatso-Pixie-Dust <kgothatso.manzini@gmail.com>
2025-02-18 13:53:10 +0200

    open image and file

--- Load File.py
+++ Load File.py
@@ -1,3 +1,22 @@
-import csv
-from pathlib import Path
-import argparse
+import openpyxl
+from archive import *
+from openpyxl.drawing.image import Image
+import os 
+def link_images_to_excel(images_info,excel_file):
+    if os.path.exists(excel_file):
+        workbook = openpyxl.load_workbook(excel_file)
+    else:
+        workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    for cell_info in images_info:
+        cell , image_path = cell_info['cell'], cell_info['image_path']
+        if os.path.exists(image_path):
+            img = Image(image_path)
+            img.anchor = cell
+            sheet.add_image(img)
+
+            sheet[cell].value = 'Image for {cell}'
+        else:
+            print(f'Image not found for {cell}: {image_path}')
+    workbook.save(excel_file)
+
